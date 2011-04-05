c008ff2a5ce2179f9cc71ab7b76ed524c046d1b5
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2011-04-05 15:16:40 +0200

    Fixed Firefox 4 support.

--- components/CoapProtocolHandler.js
+++ components/CoapProtocolHandler.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Institute for Pervasive Computing, ETH Zurich.
+ * Copyright (c) 2011, Institute for Pervasive Computing, ETH Zurich.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,26 +34,22 @@
  *
  * \author  Matthias Kovatsch <kovatsch@inf.ethz.ch>\author
  */
-
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Ce = Components.Exception;
                                 
-const CLASS_ID = Components.ID("{6ffeeb10-91cc-0854-a554-81cf891ced50}");
 
 // Array allows for multiple schemes (e.g., coap and coaps for coap with DTLS)
+const CLASS_ID = [Components.ID("{6ffeeb10-91cc-0854-a554-81cf891ced50}")];
 const CLASS_SCHEME = ["coap"];
 const CLASS_DEFAULT_PORT = [61616];
 const CLASS_NAME = ["CoAP protocol"];
 
 /**
- * Handles the LinkPassword protocols
+ * Handles the CoAP protocol
  */
-function CoapProtocolHandler() {
-}
+function CoapProtocolHandler() {}
 
 CoapProtocolHandler.prototype = {
+	classID: CLASS_ID[0],
+	
 	scheme: CLASS_SCHEME[0],
 	defaultPort : CLASS_DEFAULT_PORT[0],
 
@@ -72,35 +68,28 @@ CoapProtocolHandler.prototype = {
 		 * blah:///foo/bar => blah://foo/bar
 		 */
 		
-		var uri = Cc["@mozilla.org/network/standard-url;1"].createInstance(Ci.nsIStandardURL);
-		uri.init(Ci.nsIStandardURL.URLTYPE_AUTHORITY, this.defaultPort, aSpec, aCharset, aBaseURI);
+		try {
 		
-		return uri;
+			var uri = Components.classes["@mozilla.org/network/standard-url;1"].createInstance(Components.interfaces.nsIStandardURL);
+			uri.init(Components.interfaces.nsIStandardURL.URLTYPE_AUTHORITY, this.defaultPort, aSpec, aCharset, aBaseURI);
+			
+			return uri;
+		
+		} catch (ex) {
+			dump('ERROR: CoapProtocolHandler.newURI ['+ex+']\n');
+		}
 	},
 
 	newChannel : function(aInputURI) {
-		var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-		return ioService.newChannel("chrome://copper/content/copper.xul", null, null);
+		try {
+			var ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+			return ioService.newChannel("chrome://copper/content/copper.xul", null, null);
+			
+		} catch (ex) {
+			dump('ERROR: CoapProtocolHandler.newChannel ['+ex+']\n');
+		}
 	},
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
   /**
    * The QueryInterface method provides runtime type discovery.
    * More: http://developer.mozilla.org/en/docs/nsISupports
@@ -108,13 +97,14 @@ CoapProtocolHandler.prototype = {
    * @return the resulting interface pointer.
    */
   QueryInterface : function(aIID) {
-    if (!aIID.equals(Ci.nsIProtocolHandler) &&
-        !aIID.equals(Ci.nsISupports)) {
-      throw Cr.NS_ERROR_NO_INTERFACE;
+    if (!aIID.equals(Components.interfaces.nsIProtocolHandler) &&
+        !aIID.equals(Components.interfaces.nsISupports)) {
+      throw Components.results.NS_ERROR_NO_INTERFACE;
     }
 
     return this;
   }
+
 };
 
 /**
@@ -122,86 +112,104 @@ CoapProtocolHandler.prototype = {
  * classes without specifying a concrete class type.
  * More: http://developer.mozilla.org/en/docs/nsIFactory
  */
-var CoapProtocolHandlerFactory = {
-  createInstance: function (aOuter, aIID) {
-    if (null != aOuter) {
-      throw Cr.NS_ERROR_NO_AGGREGATION;
-    }
-    return (new CoapProtocolHandler()).QueryInterface(aIID);
-  }
+var CoapProtocolHandlerFactory = function() {};
+
+CoapProtocolHandlerFactory.prototype = {
+	createInstance: function (aOuter, aIID) {
+		if (null != aOuter) {
+			throw Components.results.NS_ERROR_NO_AGGREGATION;
+		}
+	    return (new CoapProtocolHandler()).QueryInterface(aIID);
+	}
 };
 
 /**
+ * Firefox 3.x
  * The nsIModule interface must be implemented by each XPCOM component. It is
  * the main entry point by which the system accesses an XPCOM component.
  * More: http://developer.mozilla.org/en/docs/nsIModule
  */
 var CoapProtocolHandlerModule = {
-  /**
-   * When the nsIModule is discovered, this method will be called so that any
-   * setup registration can be preformed.
-   * @param aCompMgr the global component manager.
-   * @param aLocation the location of the nsIModule on disk.
-   * @param aLoaderStr opaque loader specific string.
-   * @param aType loader type being used to load this module.
-   */
-  registerSelf : function(aCompMgr, aLocation, aLoaderStr, aType) {
-    aCompMgr.QueryInterface(Ci.nsIComponentRegistrar);
-    for (var i in CLASS_SCHEME) {
-      aCompMgr.registerFactoryLocation(CLASS_ID, CLASS_NAME[i], "@mozilla.org/network/protocol;1?name="+CLASS_SCHEME[i], aLocation, aLoaderStr, aType);
-    }
-  },
+	/**
+	 * When the nsIModule is discovered, this method will be called so that any
+	 * setup registration can be preformed.
+	 * @param aCompMgr the global component manager.
+	 * @param aLocation the location of the nsIModule on disk.
+	 * @param aLoaderStr opaque loader specific string.
+	 * @param aType loader type being used to load this module.
+	 */
+	registerSelf : function(aCompMgr, aLocation, aLoaderStr, aType) {
+		aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+		for (var i in CLASS_SCHEME) {
+			aCompMgr.registerFactoryLocation(CLASS_ID[i], CLASS_NAME[i], "@mozilla.org/network/protocol;1?name="+CLASS_SCHEME[i], aLocation, aLoaderStr, aType);
+		}
+	},
 
-  /**
-   * When the nsIModule is being unregistered, this method will be called so
-   * that any cleanup can be preformed.
-   * @param aCompMgr the global component manager.
-   * @param aLocation the location of the nsIModule on disk.
-   * @param aLoaderStr opaque loader specific string.
-   */
-  unregisterSelf : function (aCompMgr, aLocation, aLoaderStr) {
-    aCompMgr.QueryInterface(Ci.nsIComponentRegistrar);
-    aCompMgr.unregisterFactoryLocation(CLASS_ID, aLocation);
-  },
+	/**
+	 * When the nsIModule is being unregistered, this method will be called so
+	 * that any cleanup can be preformed.
+	 * @param aCompMgr the global component manager.
+	 * @param aLocation the location of the nsIModule on disk.
+	 * @param aLoaderStr opaque loader specific string.
+	 */
+	unregisterSelf : function (aCompMgr, aLocation, aLoaderStr) {
+		aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+		for (var i in CLASS_SCHEME) {
+			aCompMgr.unregisterFactoryLocation(CLASS_ID[i], aLocation);
+		}
+	},
 
-  /**
-   * This method returns a class object for a given ClassID and IID.
-   * @param aCompMgr the global component manager.
-   * @param aClass the ClassID of the object instance requested.
-   * @param aIID the IID of the object instance requested.
-   * @return the resulting interface pointer.
-   * @throws NS_ERROR_NOT_IMPLEMENTED if aIID is inadequate.
-   * @throws NS_ERROR_NO_INTERFACE if the interface is not found.
-   */
-  getClassObject : function(aCompMgr, aClass, aIID) {
-    if (!aIID.equals(Ci.nsIFactory)) {
-      throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-    }
+	/**
+	 * This method returns a class object for a given ClassID and IID.
+	 * @param aCompMgr the global component manager.
+	 * @param aClass the ClassID of the object instance requested.
+	 * @param aIID the IID of the object instance requested.
+	 * @return the resulting interface pointer.
+	 * @throws NS_ERROR_NOT_IMPLEMENTED if aIID is inadequate.
+	 * @throws NS_ERROR_NO_INTERFACE if the interface is not found.
+	 */
+	getClassObject : function(aCompMgr, aClass, aIID) {
+		if (!aIID.equals(Components.interfaces.nsIFactory)) {
+			throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+		}
 
-    if (aClass.equals(CLASS_ID)) {
-      return CoapProtocolHandlerFactory;
-    }
+		if (aClass.equals(CLASS_ID[0])) {
+			return new CoapProtocolHandlerFactory();
+		}
 
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  },
+		throw Components.results.NS_ERROR_NO_INTERFACE;
+	},
 
-  /**
-   * This method may be queried to determine whether or not the component
-   * module can be unloaded by XPCOM.
-   * @param aCompMgr the global component manager.
-   * @return true if the module can be unloaded by XPCOM. false otherwise.
-   */
-  canUnload: function(aCompMgr) {
-    return true;
-  }
+	/**
+	 * This method may be queried to determine whether or not the component
+	 * module can be unloaded by XPCOM.
+	 * @param aCompMgr the global component manager.
+	 * @return true if the module can be unloaded by XPCOM. false otherwise.
+	 */
+	canUnload: function(aCompMgr) {
+		return true;
+	}
 };
 
 /**
- * Initial entry point.
+ * Initial entry point Firefox 3.x
  * @param aCompMgr the global component manager.
  * @param aFileSpec component file.
  * @return the module for the service.
  */
 function NSGetModule(aCompMgr, aFileSpec) {
-  return CoapProtocolHandlerModule;
+	return CoapProtocolHandlerModule;
+}
+
+/**
+ * Initial entry point Firefox 4
+ * @param cid the class ID from the manifest.
+ * @return the module for the service.
+ */
+function NSGetFactory (cid) {
+	for(var i in CLASS_ID) {
+		if (cid.equals(CLASS_ID[0])) {
+			return new CoapProtocolHandlerFactory();
+		}
+	}
 }
