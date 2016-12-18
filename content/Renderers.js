a5681a6f8321c68079d621c98a3b1eae629957cd
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2016-12-18 20:51:38 +0100

    Release 1.0.1

--- content/Renderers.js
+++ content/Renderers.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, Institute for Pervasive Computing, ETH Zurich.
+ * Copyright (c) 2016, Institute for Pervasive Computing, ETH Zurich.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
